d69c6f41ff3877a1c55bbf1bf1be22bd2fd9cc15
Mathias <mathias.blak@gmail.com>
2022-04-05 14:34:26 +0200

    speak! inside Main

--- src/demo/Dog.java
+++ src/demo/Dog.java
@@ -4,6 +4,6 @@ public class Dog extends Animal{
   @Override
   public void makeSound() {
     System.out.println("WOWOW");
-    super.make.Sound();
+    super.makeSound();
   }
 }
--- src/demo/Main.java
+++ src/demo/Main.java
@@ -3,6 +3,7 @@ package demo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        new Dog().makeSound();
+        new Cat().makeSound();
     }
 }
