d012119985967368952c77c222cc86d99a5625b9
Mathias <mathias.blak@gmail.com>
2022-04-05 14:17:30 +0200

    sound

---
+++ src/demo/Animal.java
@@ -0,0 +1,8 @@
+package demo;
+
+abstract class Animal {
+  String name;
+  public void makeSound(){
+    System.out.println();
+  }
+}
--- src/demo/Dog.java
+++ src/demo/Dog.java
@@ -4,6 +4,6 @@ public class Dog extends Animal{
   @Override
   public void makeSound() {
     System.out.println("WOWOW");
-    super.makeSound();
+    super.make.Sound();
   }
 }
